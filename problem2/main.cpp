3384a8d1f53666aadff40b15f8f3143956089be7
danetteCafe <louis.viot@protonmail.com>
2019-08-03 11:09:40 +0200

    better version of problem 2

--- problem2/main.cpp
+++ problem2/main.cpp
@@ -1,2 +1,2 @@
 #include <iostream>
-int main() {int f_=1, f=1;int r=0;do {if (f%2==0) r+=f;int aux=f;f+=f_;f_=aux;} while (f<4e6);std::cout << "r=" << r << std::endl;}
+int main() {int f_=2,f=8;int r=2;do {r+=f;int aux=f; f=4*f+f_;f_=aux;} while (f<4e6);std::cout << "r=" << r << std::endl;}
